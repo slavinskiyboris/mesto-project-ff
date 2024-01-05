6660f7d3fc3683d2a12d8314297419327fd2ce15
Boris Slavinskiy <slavinskiyboris@gmail.com>
2024-01-05 23:07:00 +0300

    added functions for displaying and deleting cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,17 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+const template = document.querySelector('#card-template').content;
+const placesContainer = document.querySelector('.places');
+const list = placesContainer.querySelector('.places__list');
+function addCard (element, deleteCard) {
+    const templateCopy = template.cloneNode(true);
+    templateCopy.querySelector('.card__title').textContent = element.name;
+    templateCopy.querySelector('.card__image').src = element.link;
+    templateCopy.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+    list.append(templateCopy);
+}
+function deleteCard(element) {
+    const item = element.target.closest('.card');
+    item.remove();
+}
+initialCards.forEach(function (element) {
+    addCard(element, deleteCard);
+});
