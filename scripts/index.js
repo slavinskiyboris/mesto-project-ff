fec785a944eeeb8b618999a6324da9ac21c585e9
Boris Slavinskiy <slavinskiyboris@gmail.com>
2024-01-06 02:32:39 +0300

    fix index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,19 @@
 const template = document.querySelector('#card-template').content;
 const placesContainer = document.querySelector('.places');
 const list = placesContainer.querySelector('.places__list');
-function addCard (element, deleteCard) {
+function createCard (element, deleteCard) {
     const templateCopy = template.cloneNode(true);
     templateCopy.querySelector('.card__title').textContent = element.name;
     templateCopy.querySelector('.card__image').src = element.link;
+    templateCopy.querySelector('.card__image').alt = element.alt;
     templateCopy.querySelector('.card__delete-button').addEventListener('click', deleteCard);
-    list.append(templateCopy);
+    return templateCopy;
 }
 function deleteCard(element) {
     const item = element.target.closest('.card');
     item.remove();
 }
 initialCards.forEach(function (element) {
-    addCard(element, deleteCard);
+    const card = createCard(element, deleteCard);
+    list.append(card);
 });
